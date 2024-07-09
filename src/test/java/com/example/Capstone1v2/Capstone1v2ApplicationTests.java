af0e54056cc7c2b2ed17148cac682e6c8f0c0367
JennyyBriseno <Jennybriseno12@gmail.com>
2024-07-09 08:53:39 -0500

    initital commit

---
+++ src/test/java/com/example/Capstone1v2/Capstone1v2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Capstone1v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Capstone1v2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
