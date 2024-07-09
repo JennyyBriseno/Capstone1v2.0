67f93af58300ca79673cccbfa58f7b53d99e3490
DrUkulele <tlpriest@ymail.com>
2024-07-09 09:56:48 -0500

    Chnaged the repositry

--- src/main/java/com/example/Capstone1v2/repositories/TransactionRepository.java
+++ src/main/java/com/example/Capstone1v2/repositories/TransactionRepository.java
@@ -4,17 +4,18 @@ import com.example.Capstone1v2.models.Transactions;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transactions, Long> {
 
-    List<Transactions> findByDate(String date);
+    List<Transactions> findByDateBetween(String startDate, String endDate);
 
     List<Transactions> findByDescriptionIgnoreCase(String description);
 
     List<Transactions> findByVendorIgnoreCase(String vendor);
 
-    List<Transactions> findByAmount(double amount);
+    List<Transactions> findByAmountBetween(BigDecimal start, BigDecimal end);
 
 }
