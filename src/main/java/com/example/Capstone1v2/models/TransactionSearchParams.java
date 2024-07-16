19d275c69cc3e8565b4c4327b6dfd5cc8df0866a
JennyyBriseno <Jennybriseno12@gmail.com>
2024-07-16 09:03:03 -0500

    Last minute add

--- src/main/java/com/example/Capstone1v2/models/TransactionSearchParams.java
+++ src/main/java/com/example/Capstone1v2/models/TransactionSearchParams.java
@@ -1,11 +1,15 @@
 package com.example.Capstone1v2.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class TransactionSearchParams {
     private LocalDate startDate;
     private LocalDate endDate;
